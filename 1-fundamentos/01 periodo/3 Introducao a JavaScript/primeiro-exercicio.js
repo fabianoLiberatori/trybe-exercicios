a60616885380a5d85b196a81e348ecf840b149f8
Fabiano Liberatori <fabiano29585@gmail.com>
2023-06-01 11:35:12 -0300

    primeiro exercicio em javascript

---
+++ 1-fundamentos/01 periodo/3 Introducao a JavaScript/primeiro-exercicio.js
@@ -0,0 +1,14 @@
+let num1 = 16;
+let num2 = 5;
+
+let adição = num1 + num2;
+let subtração = num1 - num2;
+let multiplicação = num1 * num2;
+let divisão = num1 / num2;
+let módulo = num1 % num2;
+
+console.log(adição);
+console.log(subtração);
+console.log(multiplicação);
+console.log(divisão);
+console.log(módulo);

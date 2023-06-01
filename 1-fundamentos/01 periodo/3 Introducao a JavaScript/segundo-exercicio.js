73ffff71fcd70c30fade5eb87d53cda3e9827bbb
Fabiano Liberatori <fabiano29585@gmail.com>
2023-06-01 12:00:23 -0300

    segundo exercicio em javascript

---
+++ 1-fundamentos/01 periodo/3 Introducao a JavaScript/segundo-exercicio.js
@@ -0,0 +1,11 @@
+let num1 = 4;
+let num2 = 6;
+
+
+if (num1 > num2) {
+    console.log("O maior numero " + num1 + "!");
+} else if (num1 < num2) {
+    console.log("O maior numero " + num2 + "!");
+} else {
+    console.log("os dois numeros são iguais!");
+};

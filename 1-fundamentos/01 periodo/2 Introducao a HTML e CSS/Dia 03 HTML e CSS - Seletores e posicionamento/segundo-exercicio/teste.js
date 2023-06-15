bb410417d9f4da5076e30799e21a335a5dda2672
Fabiano Liberatori <fabiano29585@gmail.com>
2023-06-15 13:00:58 -0300

    Fix: Arquivos anterios e mais dia 3 do exercio DOM JS

---
+++ 1-fundamentos/01 periodo/2 Introducao a HTML e CSS/Dia 03 HTML e CSS - Seletores e posicionamento/segundo-exercicio/teste.js
@@ -0,0 +1,53 @@
+
+// function verifyPalindrome(palavra) {
+//  let separar = palavra.split('');
+//     for ( let index = (separar.length -1); index >= 0; index -= 1) {
+//      if ()
+//     }
+//  return palavraInvertida;
+// }
+
+// console.log(verifyPalindrome('orara'));
+
+
+
+
+
+// function reverseString(str) {
+    
+//     let splitString = str.split("");
+ 
+//     let reverseArray = splitString.reverse();
+ 
+//     let joinArray = reverseArray.join("");
+
+//     return joinArray;
+// }
+ 
+// reverseString("hello");
+
+
+
+// let pala = "juliana";
+// let vra = pala.split('');
+// let rev = vra.reverse();
+// console.log(rev);
+
+
+
+// function verifyPalindrome (palavra) {
+//     let separar = palavra.split('');
+//     let inverter = separar.reverse();
+//     let palavraInvertida = [];
+//     for ( let index = 0; index < inverter.length; index += 1) {
+//      palavraInvertida.push(inverter[index]);
+//     }
+//     if(palavraInvertida === separar){
+//         console.log(`A ${palavra} é um Palindrome !`);
+//         return palavraInvertida;
+//     } else {
+//         console.log(À ${palavra} não é um Palindrome !``);
+//     }
+// }
+
+// verifyPalindrome('Arara');
--- 1-fundamentos/01 periodo/3 Introducao a JavaScript/segundo-exercicio.js
+++ 1-fundamentos/01 periodo/3 Introducao a JavaScript/segundo-exercicio.js
@@ -1,4 +1,4 @@
-let num1 = 4;
+let num1 = 7;
 let num2 = 6;
 
 
